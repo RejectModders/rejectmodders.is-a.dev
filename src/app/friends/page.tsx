c81064932b62b7426c41e1e181f12ddf391f4ea3
RejectModders <142326052+RejectModders@users.noreply.github.com>
2025-03-10 08:23:05 -0500

    Update page.tsx

--- src/app/friends/page.tsx
+++ src/app/friends/page.tsx
@@ -1,5 +1,4 @@
-'use client'
-
+import { useEffect } from 'react'
 import { type Metadata } from 'next'
 import { SimpleLayout } from '@/components/layout/SimpleLayout'
 
@@ -13,27 +12,28 @@ export const metadata: Metadata = {
 }
 
 export default function Friends() {
+  useEffect(() => {
+    // Smooth scrolling for this page
+    document.documentElement.style.scrollBehavior = "smooth";
+
+    return () => {
+      document.documentElement.style.scrollBehavior = "auto";
+    };
+  }, []);
+
   return (
-    <>
-      <style jsx global>{`
-        /* Smooth scrolling only for this page */
-        html {
-          scroll-behavior: smooth;
-        }
-      `}</style>
-      <SimpleLayout
-        title={friendsHeadLine}
-        intro={friendsIntro}
+    <SimpleLayout
+      title={friendsHeadLine}
+      intro={friendsIntro}
+    >
+      <ul
+        role="list"
+        className="grid grid-cols-1 gap-x-8 gap-y-12 sm:grid-cols-2 lg:grid-cols-3 pb-10"
       >
-        <ul
-          role="list"
-          className="grid grid-cols-1 gap-x-8 gap-y-12 sm:grid-cols-2 lg:grid-cols-3 pb-10"
-        >
-          {friends.map((friend) => (
-            <FriendCard key={friend.name} friend={friend} />
-          ))}
-        </ul>
-      </SimpleLayout>
-    </>
+        {friends.map((friend) => (
+          <FriendCard key={friend.name} friend={friend} />
+        ))}
+      </ul>
+    </SimpleLayout>
   )
 }
