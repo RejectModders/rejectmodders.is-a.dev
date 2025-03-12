19d88f9fdd1a07fea82ecaf74ba81d36a7580992
RejectModders <142326052+RejectModders@users.noreply.github.com>
2025-03-12 10:15:59 -0500

    Update GithubProjectCard.tsx

--- src/components/project/GithubProjectCard.tsx
+++ src/components/project/GithubProjectCard.tsx
@@ -6,14 +6,14 @@ import { ProjectItemType } from '@/config/infoConfig'
 import { utm_source } from '@/config/siteConfig'
 import Link from 'next/link'
 
-
 export function GithubProjectCard({ project, titleAs }: { project: ProjectItemType, titleAs?: keyof JSX.IntrinsicElements }) {
   const utmLink = `https://${project.link.href}?utm_source=${utm_source}`
   let Component = titleAs ?? 'h2'
+  
   return (
     <li className='group relative flex flex-col items-start h-full'>
-      <div className="relative flex flex-col justify-between h-full w-full py-5  px-6 rounded-2xl border border-muted-foreground/20 shadow-sm transition-all group-hover:scale-[1.03] group-hover:shadow-md group-hover:bg-muted/5">
-        <div className=''>
+      <div className="relative flex flex-col justify-between h-full w-full py-5 px-6 rounded-2xl border border-muted-foreground/20 shadow-sm transition-all group-hover:scale-[1.03] group-hover:shadow-md group-hover:bg-muted/5">
+        <div>
           <div className='flex flex-col sm:flex-row justify-center sm:justify-start items-start sm:items-center gap-2'>
             <BookOpen size={20} weight="duotone" />
             <Component className="text-sm font-semibold tracking-tight">
@@ -27,13 +27,13 @@ export function GithubProjectCard({ project, titleAs }: { project: ProjectItemTy
 
         <div className="relative z-10 mt-auto pt-4">
           <div className='flex flex-row items-center gap-2 text-xs font-semibold opacity-80'>
-            { project.gitStars && (
+            { project.gitStars !== undefined && (
               <>
                 <Star size={16} weight="duotone" /> 
                 {project.gitStars}
               </>
             )}
-            { project.gitForks && (
+            { project.gitForks !== undefined && (
               <>
                 <GitFork size={16} weight="duotone" /> 
                 {project.gitForks}
@@ -41,6 +41,7 @@ export function GithubProjectCard({ project, titleAs }: { project: ProjectItemTy
             )}
           </div>
         </div>
+        
         <Link
           href={utmLink}
           target='_blank'
